ac9b206c8b7cd563ea27182f02c6a58ef8c210b3
reiss-josh <reiss.joshuae@gmail.com>
2020-02-13 13:52:14 -0800

    Started work on Marching Squares

---
+++ Assets/Scripts/VoxelGrid.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]
+public class VoxelGrid : MonoBehaviour
+{
+	public GameObject voxelPrefab;
+	public int resolution;
+
+	private bool[] voxels;
+	private float voxelSize;
+
+	public void Initialize(int resolution, float size)
+	{
+		this.resolution = resolution;
+		voxelSize = size / resolution;
+		voxels = new bool[resolution * resolution];
+
+		for (int i = 0, y = 0; y < resolution; y++)
+		{
+			for (int x = 0; x < resolution; x++, i++)
+			{
+				CreateVoxel(i, x, y);
+			}
+		}
+	}
+
+	private void CreateVoxel(int i, int x, int y)
+	{
+		GameObject newVoxel = Instantiate(voxelPrefab) as GameObject;
+		newVoxel.transform.parent = transform;
+		newVoxel.transform.localPosition = new Vector3((x + 0.5f) * voxelSize, (y + 0.5f) * voxelSize);
+		newVoxel.transform.localScale = Vector3.one * voxelSize;
+
+		Vector3 voxelScale = Vector3.one * voxelSize * 0.9f;
+		newVoxel.transform.localScale = voxelScale;
+	}
+}
---
+++ Assets/Scripts/VoxelMap.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoxelMap : MonoBehaviour
+{
+
+	public float size = 2f;
+	public int voxelResolution = 8;
+	public int chunkResolution = 2;
+	public VoxelGrid voxelGridPrefab;
+
+	private VoxelGrid[] chunks;
+
+	private float chunkSize, voxelSize, halfSize;
+
+	private void Awake()
+	{
+		halfSize = size * 0.5f;
+		chunkSize = size / chunkResolution;
+		voxelSize = chunkSize / voxelResolution;
+
+		chunks = new VoxelGrid[chunkResolution * chunkResolution];
+		for (int i = 0, y = 0; y < chunkResolution; y++)
+		{
+			for (int x = 0; x < chunkResolution; x++, i++)
+			{
+				CreateChunk(i, x, y);
+			}
+		}
+	}
+
+	private void CreateChunk(int i, int x, int y)
+	{
+		VoxelGrid chunk = Instantiate(voxelGridPrefab) as VoxelGrid;
+		chunk.Initialize(voxelResolution, chunkSize);
+		chunk.transform.parent = transform;
+		chunk.transform.localPosition = new Vector3(x * chunkSize - halfSize, y * chunkSize - halfSize);
+		chunks[i] = chunk;
+	}
+}
