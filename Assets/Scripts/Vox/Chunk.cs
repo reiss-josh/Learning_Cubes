d8d84b366e327dc047da5ec617521f1fbf4532e5
Josh <dash219@gmail.com>
2020-02-28 10:43:58 -0800

    Partially converted to GPU

--- Assets/Scripts/Vox/Chunk.cs
+++ Assets/Scripts/Vox/Chunk.cs
@@ -5,10 +5,38 @@ using UnityEngine;
 [SelectionBase]
 public class Chunk : MonoBehaviour
 {
+	struct Triangle
+	{
+#pragma warning disable 649 // disable unassigned variable warning
+		public Vector3 a;
+		public Vector3 b;
+		public Vector3 c;
+
+		public Vector3 this[int i]
+		{
+			get
+			{
+				switch (i)
+				{
+					case 0:
+						return a;
+					case 1:
+						return b;
+					default:
+						return c;
+				}
+			}
+		}
+	}
+
+
 	public int resolution, size;
 	public float threshold;
-	public float[] vValues;
-	public Vector3[] vCoords;
+	public Vector4[] voxels;
+	public ComputeShader shader;
+	private ComputeBuffer pointsBuffer;
+	private ComputeBuffer triCountBuffer;
+	private ComputeBuffer triangleBuffer;
 
 	private int resSqr;
 	private float voxelSize, vHSize;
@@ -32,8 +60,7 @@ public class Chunk : MonoBehaviour
 		theMeshCollider = GetComponent<MeshCollider>();
 		theMeshCollider.sharedMesh = null;
 
-		vCoords = new Vector3[resolution * resSqr];
-		vValues = new float[resolution * resSqr];
+		voxels = new Vector4[resolution * resSqr];
 		float newVal, currMin = 0, currMax = 0;
 
 		//create voxels at all points in chunk
@@ -41,9 +68,9 @@ public class Chunk : MonoBehaviour
 		for (int z = 0; z < resolution; z++) {
 			for (int y = 0; y < resolution; y++) {
 				for (int x = 0; x < resolution; x++, i++) {
-					vCoords[i] = new Vector3(x+0.5f, y+0.5f, z+0.5f)*voxelSize;
-					newVal = Density.Sample(vCoords[i] + transform.localPosition);
-					vValues[i] = newVal;
+					voxels[i] = new Vector4(x+0.5f, y+0.5f, z+0.5f,0)*voxelSize;
+					newVal = Density.Sample(ToVector3(voxels[i]) + transform.localPosition);
+					voxels[i].w = newVal;
 					if (newVal < currMin) currMin = newVal;
 					else if (newVal > currMax) currMax = newVal;
 				}
@@ -66,16 +93,71 @@ public class Chunk : MonoBehaviour
 		theMeshCollider.sharedMesh = mesh;
 	}
 
+	void OnDestroy()
+	{
+		if (Application.isPlaying)
+		{
+			ReleaseBuffers();
+		}
+	}
+
+	void ReleaseBuffers()
+	{
+		if (triangleBuffer != null)
+		{
+			triangleBuffer.Release();
+			pointsBuffer.Release();
+			triCountBuffer.Release();
+		}
+	}
+
 	private void Triangulate()
 	{
-		vertices.Clear();
-		triangles.Clear();
+
+		//TriangulateCellRows();
+		int numPoints = voxels.Length;
+		int maxTriangleCount = numPoints * 5;
+		triangleBuffer = new ComputeBuffer(maxTriangleCount, sizeof(float) * 3 * 3, ComputeBufferType.Append);
+		pointsBuffer = new ComputeBuffer(numPoints, sizeof(float) * 4);
+		triCountBuffer = new ComputeBuffer(1, sizeof(int), ComputeBufferType.Raw);
+
+		pointsBuffer.SetData(voxels, 0, 0, numPoints);
+
+		triangleBuffer.SetCounterValue(0);
+		shader.SetBuffer(0, "voxels", pointsBuffer);
+		shader.SetBuffer(0, "tris", triangleBuffer);
+		shader.SetInt("numPointsPerAxis", resolution);
+		shader.SetFloat("isoLevel", threshold);
+
+		shader.Dispatch(0, resolution, resolution, resolution);
+
+		// Get number of triangles in the triangle buffer
+		ComputeBuffer.CopyCount(triangleBuffer, triCountBuffer, 0);
+		int[] triCountArray = { 0 };
+		triCountBuffer.GetData(triCountArray);
+		int numTris = triCountArray[0];
+
+		// Get triangle data from shader
+		Triangle[] tris = new Triangle[numTris];
+		triangleBuffer.GetData(tris, 0, 0, numTris);
+
 		mesh.Clear();
 
-		TriangulateCellRows();
+		var vertices = new Vector3[numTris * 3];
+		var meshTriangles = new int[numTris * 3];
 
-		mesh.vertices = vertices.ToArray();
-		mesh.triangles = triangles.ToArray();
+		for (int i = 0; i < numTris; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				meshTriangles[i * 3 + j] = i * 3 + j;
+				vertices[i * 3 + j] = tris[i][j];
+			}
+		}
+		mesh.vertices = vertices;
+		mesh.triangles = meshTriangles;
+
+		mesh.RecalculateNormals();
 	}
 
 	private int[] genDefaultVerts(int i)
@@ -108,7 +190,7 @@ public class Chunk : MonoBehaviour
 	{
 		int cellType = 0;
 		for (int i = 0, t = 1; i < CubeVerts.Length; i++, t = t * 2) {
-			if (vValues[CubeVerts[i]] > threshold)
+			if (voxels[CubeVerts[i]].w > threshold)
 			{
 				if (i == 2 || i == 6)
 					cellType |= t * 2;
@@ -119,7 +201,7 @@ public class Chunk : MonoBehaviour
 			}
 		}
 
-		Vector3 v0pos = vCoords[CubeVerts[0]];
+		Vector3 v0pos = ToVector3(voxels[CubeVerts[0]]);
 		int[] stitchEdges = triTable[cellType];
 		for (int i = 0; i < 15; i+=3) {
 			if (stitchEdges[i] < 0) {
@@ -146,5 +228,10 @@ public class Chunk : MonoBehaviour
 		triangles.Add(vertexIndex + 2);
 		triangles.Add(vertexIndex + 1);
 		triangles.Add(vertexIndex + 0);
+	}
+
+	public static Vector3 ToVector3(Vector4 parent)
+	{
+		return new Vector3(parent.x, parent.y, parent.z);
 	}
 }
