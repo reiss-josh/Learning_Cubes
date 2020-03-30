a20795969cbed64b8f209f50c4c8ac3c81d73560
reiss-josh <reiss.joshuae@gmail.com>
2020-03-29 18:51:00 -0700

    Attempted Fix

--- Assets/Scripts/Vox/VoxelMap.cs
+++ Assets/Scripts/Vox/VoxelMap.cs
@@ -4,26 +4,48 @@ using UnityEngine;
 
 public class VoxelMap : MonoBehaviour
 {
-	public float threshold = 0.5f;
-	public float size = 2f;
-	public int voxelResolution = 8;
-	public int chunkResolution = 2;
-	public int chunkYResolution = 2;
-	public Chunk chunkPrefab;
+	public float threshold = 0.5f;	//threshold for points in space
+	public float voxelSize = 2f;			//size of a voxel across each axis
+	public int voxelResolution = 8; //num voxels per chunk, per axis
+	public int chunkResolution = 2; //num chunks per axis
+	public Chunk chunkPrefab;       //prefab for chunk
 
-	private Chunk[] chunks;
-	private float chunkSize, chunkYSize, voxelSize, halfSize;
+	public ComputeShader shader;			//shader??
+	public ComputeShader densityShader;		//density shader -- generates "w" for each voxel
+	private ComputeBuffer pointsBuffer;		//stores all points in space
+	private ComputeBuffer triCountBuffer;	//keeps count of number of tris in a chunk
+	private ComputeBuffer triangleBuffer;   //stores actual tris in a chunk
 
+	private int numPoints;                  //stores number of points
+	private int maxTriangleCount;           //determines max # of triangles in a chunk
+	private int chunkResSqr, voxelResSqr;   //res^2
+	private static int[][] triTable = Lookup.triangulation;
+	private static Vector3[] edgeTable = Lookup.edges;
+
+	private Chunk[] chunks;	//array of chunks
+	private float chunkSize, voxelHalfSize;
+
+	//setup variables for chunk generation
 	private void Awake()
 	{
-		halfSize = size * 0.5f;
-		chunkSize = size / chunkResolution;
-		chunkYSize = size / chunkYResolution;
-		voxelSize = chunkSize / voxelResolution;
+		chunkResSqr = chunkResolution * chunkResolution;
+		voxelResSqr = voxelResolution * voxelResolution;
+		voxelHalfSize = voxelSize * 0.5f;	 //used to find midpoint of a chunk
+		chunkSize = voxelSize * voxelResolution;      //size of a chunk, per axis
+		Debug.Log(chunkSize);
+		Debug.Log(voxelSize);
 		chunks = new Chunk[chunkResolution * chunkResolution * chunkResolution];
 
+		GenerateChunks();
+	}
+
+	//create all chunks in mesh
+	private void GenerateChunks()
+	{
+		//i is current chunk ID
+		//x,y,z is integer chunk position in world mesh
 		for (int i = 0, z = 0; z < chunkResolution; z++) {
-			for (int y = 0; y < chunkYResolution; y++) {
+			for (int y = 0; y < chunkResolution; y++) {
 				for (int x = 0; x < chunkResolution; x++, i++) {
 					CreateChunk(i, x, y, z);
 				}
@@ -31,12 +53,133 @@ public class VoxelMap : MonoBehaviour
 		}
 	}
 
+	//generate a single chunk
+	//takes as input a chunk id and x,y,z position in world mesh
 	private void CreateChunk(int i, int x, int y, int z)
 	{
 		Chunk chunk = Instantiate(chunkPrefab) as Chunk;
-		chunk.transform.parent = transform;
-		chunk.transform.position = new Vector3(x * chunkSize - halfSize, y * chunkSize - halfSize, z * chunkSize - halfSize);
-		chunk.Initialize(voxelResolution, chunkSize, threshold);
 		chunks[i] = chunk;
+
+		chunk.transform.parent = transform;
+		chunk.transform.position = new Vector3(x*chunkSize, y*chunkSize, z*chunkSize); //placeholder -- assumes only one chunk
+
+		chunk.Initialize(x,y,z);
+		Triangulate(i);
+	}
+
+	private int chunkIDFromCoord(int x, int y, int z)
+	{
+		return x + (y * chunkResolution) + (z * chunkResSqr);
+	}
+
+	private void Triangulate(int iD)
+	{
+		Chunk currChunk = chunks[iD];
+		currChunk.chunkMeshCollider.sharedMesh = null;
+		BuildBuffers();
+		densityShader.SetBuffer(0, "voxels", pointsBuffer);
+		densityShader.SetInt("voxRes", voxelResolution);
+		densityShader.SetFloat("voxMid", voxelResolution * 0.5f); //EXPECTED TO BE AN INTEGER
+		densityShader.SetFloat("chunkMid", chunkResolution * 0.5f); //EXPECTED TO BE AN INTEGER
+		densityShader.SetFloat("tfx", currChunk.transform.localPosition.x*10);
+		densityShader.SetFloat("tfy", currChunk.transform.localPosition.y*10);
+		densityShader.SetFloat("tfz", currChunk.transform.localPosition.z*10);
+		densityShader.SetFloat("vS", voxelSize * 10);
+		densityShader.SetFloat("cS", chunkSize * 10);
+		densityShader.Dispatch(0, voxelResolution, voxelResolution, voxelResolution);
+
+		//look inside the points!
+		Vector4[] beep = new Vector4[voxelResolution * voxelResSqr];
+		pointsBuffer.GetData(beep);
+		for (int i = 0; i < beep.Length; i++)
+		{
+			Debug.Log(beep[i]);
+			GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			cube.transform.position = ToVector3(beep[i]);
+			cube.transform.localScale = new Vector3(voxelSize*0.5f, voxelSize*0.5f, voxelSize*0.5f);
+		}
+
+		triangleBuffer.SetCounterValue(0);
+		shader.SetBuffer(0, "voxels", pointsBuffer);
+		shader.SetBuffer(0, "tris", triangleBuffer);
+		shader.SetInt("resolution", voxelResolution);
+		shader.SetFloat("isoLevel", threshold);
+		shader.SetFloat("vHSize", voxelHalfSize);
+
+		shader.Dispatch(0, voxelResolution, voxelResolution, voxelResolution);
+
+		// Get number of triangles in the triangle buffer
+		ComputeBuffer.CopyCount(triangleBuffer, triCountBuffer, 0);
+		int[] triCountArray = { 0 };
+		triCountBuffer.GetData(triCountArray);
+		int numTris = triCountArray[0];
+
+		// Get triangle data from shader
+		Triangle[] tris = new Triangle[numTris];
+		triangleBuffer.GetData(tris, 0, 0, numTris);
+
+		currChunk.chunkMesh.Clear();
+
+		var vertices = new Vector3[numTris * 3];
+		var chunkMeshTriangles = new int[numTris * 3];
+
+		for (int i = 0; i < numTris; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				chunkMeshTriangles[i * 3 + j] = i * 3 + j;
+				vertices[i * 3 + j] = tris[i][j];
+			}
+		}
+		currChunk.chunkMesh.vertices = vertices;
+		currChunk.chunkMesh.triangles = chunkMeshTriangles;
+
+		currChunk.chunkMesh.RecalculateNormals();
+		currChunk.chunkMeshCollider.sharedMesh = currChunk.chunkMesh;
+		ReleaseBuffers();
+	}
+
+	public static Vector3 ToVector3(Vector4 parent)
+	{
+		return new Vector3(parent.x, parent.y, parent.z);
+	}
+
+	void BuildBuffers()
+	{
+		numPoints = voxelResolution * voxelResSqr;
+		maxTriangleCount = numPoints * 5 * 2;
+		triangleBuffer = new ComputeBuffer(maxTriangleCount, sizeof(float) * 3 * 3, ComputeBufferType.Append);
+		pointsBuffer = new ComputeBuffer(numPoints, sizeof(float) * 4);
+		triCountBuffer = new ComputeBuffer(1, sizeof(int), ComputeBufferType.Raw);
+	}
+
+	void ReleaseBuffers()
+	{
+		if (triangleBuffer != null)
+		{
+			triangleBuffer.Release();
+			pointsBuffer.Release();
+			triCountBuffer.Release();
+		}
+	}
+
+	struct Triangle
+	{
+		#pragma warning disable 649 // disable unassigned variable warning
+		public Vector3 a; //position of first vert
+		public Vector3 b;
+		public Vector3 c;
+		public Vector3 this[int i] {
+			get {
+				switch (i) {
+					case 0:
+						return a;
+					case 1:
+						return b;
+					default:
+						return c;
+				}
+			}
+		}
 	}
 }
