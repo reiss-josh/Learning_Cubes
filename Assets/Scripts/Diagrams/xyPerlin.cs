0dd752c1e5a46619697ba4233c32f674d4633d77
reiss-josh <reiss.joshuae@gmail.com>
2020-04-02 14:51:34 -0700

    worked on visualizations

---
+++ Assets/Scripts/Diagrams/xyPerlin.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class xyPerlin : MonoBehaviour
+{
+    public Vector2Int xyLims;
+    public Vector2Int xyMins;
+    public float threshold;
+    private Vector2Int xyRange;
+    private GameObject[] cubeHolder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xyRange = new Vector2Int(xyLims.x - xyMins.x, xyLims.y - xyMins.y);
+        cubeHolder = new GameObject[xyRange.x * xyRange.y];
+        int currInd = 0;
+        for (int j = xyMins.y; j < xyLims.y; j++)
+        {
+            for (int i = xyMins.x; i < xyLims.x; i++)
+            {
+                GameObject freshCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cubeHolder[currInd] = freshCube;
+                freshCube.transform.SetParent(transform);
+                freshCube.transform.position = new Vector2(i, j);
+                currInd++;
+            }
+        }
+
+        for (int i = 0; i < cubeHolder.Length; i++)
+        {
+            var coords = cubeHolder[i].transform.position;
+            var x = coords.x;
+            var y = coords.y;
+            var w = Mathf.PerlinNoise(x/5, y/5);
+            Debug.Log(w);
+            if (w > threshold)
+                cubeHolder[i].GetComponent<Renderer>().material.color = Color.black;
+            else
+                cubeHolder[i].GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+}
---
+++ Assets/Scripts/Diagrams/xyPlacement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class xyPlacement : MonoBehaviour
+{
+    public Vector2Int xyLims;
+    public Vector2Int xyMins;
+    private Vector2Int xyRange;
+    private GameObject[] cubeHolder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xyRange = new Vector2Int (xyLims.x - xyMins.x, xyLims.y - xyMins.y);
+        cubeHolder = new GameObject[xyRange.x * xyRange.y];
+        int currInd = 0;
+        for(int j = xyMins.y; j < xyLims.y; j++)
+        {
+            for(int i = xyMins.x; i < xyLims.x; i++)
+            {
+                GameObject freshCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cubeHolder[currInd] = freshCube;
+                freshCube.transform.SetParent(transform);
+                freshCube.transform.position = new Vector2 ( i, j);
+                currInd++;
+            }
+        }
+
+        for(int i = 0; i < cubeHolder.Length; i++)
+        {
+            var coords = cubeHolder[i].transform.position;
+            var x = coords.x;
+            var y = coords.y;
+            if(x < -25 || x > 25 || y < -25 || y > 25){
+                cubeHolder[i].GetComponent<Renderer>().material.color = Color.black;
+            } else {
+                var bw = (Random.value > 0.5f);
+                if(!bw)
+                    cubeHolder[i].GetComponent<Renderer>().material.color = Color.black;
+                else
+                    cubeHolder[i].GetComponent<Renderer>().material.color = Color.white;
+            }
+        }
+    }
+}
